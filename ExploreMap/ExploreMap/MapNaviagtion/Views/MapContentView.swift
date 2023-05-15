43308e293eb22455c646a702cbf3a010361908f5
Sajid Shanta <Sajid@Mohammads-Mac-mini.local>
2023-05-15 16:46:24 +0600

    implemet request directions

--- ExploreMap/ExploreMap/MapNaviagtion/Views/MapContentView.swift
+++ ExploreMap/ExploreMap/MapNaviagtion/Views/MapContentView.swift
@@ -25,7 +25,7 @@ struct MapContentView: View {
             HStack {
                 TextField("Where do you want to go?", text: $tbLocationEntered)
                 Button {
-                    //
+                    findNewLocation()
                 } label: {
                     Text("Go!")
                 }
@@ -42,5 +42,45 @@ struct MapContentView: View {
 struct MapContentView_Previews: PreviewProvider {
     static var previews: some View {
         MapContentView()
+    }
+}
+
+extension MapContentView {
+    func findNewLocation() {
+        let locationEntered = tbLocationEntered
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(locationEntered, completionHandler: { (placemarks, error) -> Void in
+            if error != nil {
+                print("error at geocode. \(String(describing: error))")
+            }
+            
+            if let placemark = placemarks?.first {
+                let coordinates: CLLocationCoordinate2D = placemark.location!.coordinate
+                
+                region = MKCoordinateRegion(center: coordinates, span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))
+                
+                annotations.append(Location(name: placemark.name!, coordinate: coordinates))
+                
+                //directions
+                let request = MKDirections.Request()
+                request.source = MKMapItem(placemark: MKPlacemark(coordinate: home, addressDictionary: nil))
+                request.destination = MKMapItem(placemark: MKPlacemark(coordinate: coordinates, addressDictionary: nil))
+                request.requestsAlternateRoutes = false
+                request.transportType = .automobile // can try 'walking' or other option also
+                
+                let directions = MKDirections(request: request)
+                directions.calculate(completionHandler: { response, error in
+                    for route in (response?.routes)! {
+                        self.routeSteps = []
+                        
+                        for step in route.steps {
+                            self.routeSteps.append(RouteSteps(step: step.instructions))
+                        }
+                    }
+                })
+            }
+        })
     }
 }
